7e4fefe159bcf39303a9dd1b80e7173af1aec7de
Trisha Alvarez <trisha.alvarez@mllrdev.com>
2021-11-18 16:23:33 +0800

    style: user list layout
    ci: add moment dependency

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,17 +1,20 @@
 <template>
-    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-card no-body class="overflow-hidden border-0" style="max-width: 540px;">
         <b-row no-gutters>
-        <b-col md="6">
-            <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
-        </b-col>
-        <b-col md="6">
-            <b-card-body :title="fullName">
-            <b-card-text>
-                This is a wider card with supporting text as a natural lead-in to additional content.
-                This content is a little bit longer.
-            </b-card-text>
-            </b-card-body>
-        </b-col>
+            <b-col md="6">
+                <b-card-img :src="$props.user.picture.large" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col md="6">
+                <b-card-body>
+                    <b-card-title :class="borderColor">{{ fullName }}</b-card-title>
+                    <b-card-text>
+                        <small><i>{{ $props.user.gender }} / {{ $props.user.dob.age }} / {{ formatDate }}</i></small><br>
+                        <p class="pt-2">{{ $props.user.email }}</p>
+                        <small>{{ $props.user.phone }}</small><br>
+                        <small>{{ $props.user.cell }}</small>
+                    </b-card-text>
+                </b-card-body>
+            </b-col>
         </b-row>
   </b-card>
 </template>
@@ -19,6 +22,7 @@
 <script lang="ts">
 import { IUser } from '@/interface/user'
 import { defineComponent, PropType, computed } from 'vue'
+import moment from 'moment'
 
 export default defineComponent({
     name: 'user',
@@ -33,9 +37,34 @@ export default defineComponent({
             return props.user.name.first + ' ' + props.user.name.last
         })
 
+        const formatDate = computed((): string => {
+            return moment(String(props.user.dob.date)).format('MMMM DD, YYYY')
+        })
+
+        const borderColor = computed((): string => {
+            return (props.user.gender === 'male') ? 'header-blue' : 'header-yellow'
+        })
+
         return {
-            fullName
+            fullName,
+            formatDate,
+            borderColor
         }
     }
 })
 </script>
+
+<style scoped>
+img {
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+}
+
+h4 {
+    font-family: 'Shippori Antique', sans-serif;
+    font-size: 2rem;
+    width: fit-content;
+    margin: 0;
+    line-height: 1;
+}
+</style>
