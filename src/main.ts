1b35ac660e6a067e5aa38aa60e023924baec01f9
Trisha Alvarez <trisha.alvarez@mllrdev.com>
2021-11-18 12:22:09 +0800

    build: add bootstrap-vue-3 dependencies
    fix: remove unused about view component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+createApp(App).use(BootstrapVue3).use(router).mount('#app')
