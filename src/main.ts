e41896fca5e5c7538f1134ad229c3b3d6dd64d25
Trisha Alvarez <trisha.alvarez@mllrdev.com>
2021-11-23 13:46:24 +0800

    refactor: bootstrap imports to the plugins folder

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
 import router from './router'
-
-import './assets/styles/app.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import '@/plugins/bootstrap'
 
 createApp(App).use(BootstrapVue3).use(router).mount('#app')
---
+++ src/plugins/bootstrap.js
@@ -0,0 +1,3 @@
+import '@/assets/styles/app.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
